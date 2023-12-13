28938ebbb344fc627e3f00533d1ed765ea680bb2
Shaurya Pratap Singh <66877981+shaurya010@users.noreply.github.com>
2023-12-13 17:05:46 +0530

    Add files via upload

---
+++ RNG.py
@@ -0,0 +1,22 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute,IBMQ
+from qiskit.tools.monitor import job_monitor
+
+IBMQ.enable_account('ENTER API TOKEN HERE')
+provider = IBMQ.get_provider(hub='ibm-q')
+
+q = QuantumRegister(16,'q')
+c = ClassicalRegister(16,'c')
+circuit = QuantumCircuit(q,c)
+circuit.h(q) # Applies hadamard gate to all qubits
+circuit.measure(q,c) # Measures all qubits 
+
+backend = provider.get_backend('ibmq_qasm_simulator')
+job = execute(circuit, backend, shots=1)
+                               
+print('Executing Job...\n')                 
+job_monitor(job)
+counts = job.result().get_counts()
+
+print('RESULT: ',counts,'\n')
+print('Press any key to close')
+input()
